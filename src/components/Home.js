9f65cd400ee7810bfee22a6e83043321a2d52f1f
Luis <l-serrato@live.com>
2023-01-11 18:52:19 -0300

    requisito 5

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,86 @@
 import React, { Component } from 'react';
+import * as api from '../services/api';
 
-class home extends Component {
+export default class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      search: '',
+      categories: [],
+      result: [],
+    };
+  }
+
+  componentDidMount() {
+    api.getCategories();
+    api.getCategories().then((data) => {
+      this.setState({
+        categories: data,
+      });
+    });
+  }
+
+  inputText = ({ target }) => {
+    this.setState({
+      search: target.value,
+    });
+  };
+
+  searchButton = async () => {
+    const { search, categories } = this.state;
+    console.log(categories);
+    const apiSearch = api.getProductsFromCategoryAndQuery;
+    const response = await apiSearch('', search);
+    const result = response.results;
+    this.setState({
+      result,
+    });
+  };
+
   render() {
+    const { search, result, categories } = this.state;
+
     return (
-      <div>
-        <h1 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h1>
-      </div>
+      <>
+        <input
+          data-testid="query-input"
+          type="text"
+          onChange={ this.inputText }
+        />
+        <button
+          data-testid="query-button"
+          type="button"
+          onClick={ this.searchButton }
+        >
+          Pesquisar
+        </button>
+        {result.length < 1 ? <h1>Nenhum produto foi encontrado</h1> : (
+          <section>
+            {result.map((eachResult) => (
+              <div data-testid="product" key={ eachResult.id }>
+                <img src={ eachResult.thumbnail } alt="" />
+                <span>{eachResult.title}</span>
+                <span>
+                  { eachResult.price }
+                  { eachResult.currency_id }
+                </span>
+              </div>
+            ))}
+          </section>
+        )}
+        { search === '' ? (
+          <h1 data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h1>
+        ) : ''}
+
+        {categories.map((cat) => (
+          <button data-testid="category" type="button" key={ cat.id }>
+            {cat.name}
+          </button>
+        ))}
+      </>
     );
   }
 }
-
-export default home;
